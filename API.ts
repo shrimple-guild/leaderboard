2044bb10ea9fa02a93e7811af8b396f6d5a14e34
Appability <appable@icloud.com>
2023-08-26 15:23:31 -0700

    powder grinding event

--- API.ts
+++ API.ts
@@ -114,7 +114,7 @@ function getMetric(member: any, metric: Metric): number | undefined {
       + (member.stats?.mythos_kills ?? 0) * 3650
       + (member.leveling?.experience ?? 0) * 1000
   } else if (metric.name == "Powder Score") {
-    return totalMithril * 1.15 + totalGemstone
+    return totalMithril + totalGemstone
   } else if (metric.name == "Fishing Actions") {
     return (member.stats?.pet_milestone_sea_creatures_killed ?? 0) + (member.stats?.items_fished ?? 0)
   } else if (metric.name == "Trophy Fish Weight") {
