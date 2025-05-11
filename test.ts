d668def43eac2909939bd078c22e16bff0538d29
appable <16139460+appable0@users.noreply.github.com>
2025-05-11 10:10:14 -0700

    alligator bestiary event

--- test.ts
+++ test.ts
@@ -13,8 +13,8 @@ const database = new Database(":memory:", metrics)
 const lb = new Leaderboard(api, database)
 //const event = GuildEvent.from(eventConfig, lb)
 
-const uuid = "62e7b7bc460a45e2b5c83e56dc340acf"
-const profileName = "lime"
+const uuid = "59998433ceda41c1b0acffe7d9b33594"
+const profileName = "orange"
 
 const profiles = await api.fetchProfiles(uuid)
 const profile = profiles.find(profile => profile.cuteName.toLowerCase() == profileName)
